b1a91fcb20d47c964d1b363394d8f5481f6e040f
Mikael Brevik <mikaelbre@gmail.com>
2015-02-21 11:43:44 +0100

    Adds initial support for cursors and immutable structures as input

--- index.js
+++ index.js
@@ -7,12 +7,30 @@ var assign = require('lodash.assign');
 var elements = require('./elements');
 
 function createComponents(omniscient){
+  var scu = omniscient.shouldComponentUpdate;
   var components = {};
 
   elements.forEach(function(element){
     components[element] = omniscient(element, function(props, statics){
+      // Best approach to "magically" turn this to a child?
+      if (scu.isCursor(props) && !scu.isImmutable(props.deref())) {
+        props = {
+          children: props.deref()
+        };
+      }
+      else if (scu.isCursor(props)) {
+        props = props.deref();
+      }
+
+      if (scu.isImmutable(props)) {
+        props = props.toJS();
+      }
+
+      if (scu.isImmutable(statics)) {
+        statics = statics.toJS();
+      }
       var _props = assign({}, omit(props, 'statics'), statics);
-      return React.createElement(element, _props);
+      return React.createElement(element, _props)
     });
   });
 
